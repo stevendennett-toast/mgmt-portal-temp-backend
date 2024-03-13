3291194e06c4863464cda84937b2802608c2551d
Steve Dennett <steven.dennett@toasttab.com>
2024-03-13 11:55:19 -0400

    remove dist

--- dist/dataStore.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shiftsState = exports.schedulesState = void 0;
-const mockSchedulesData_1 = require("./MgmtSchedule/mockSchedulesData");
-const mockShiftsData_1 = require("./MgmtShift/mockShiftsData");
-exports.schedulesState = [...mockSchedulesData_1.mockSchedulesData];
-exports.shiftsState = Object.assign({}, mockShiftsData_1.mockShiftsData);
